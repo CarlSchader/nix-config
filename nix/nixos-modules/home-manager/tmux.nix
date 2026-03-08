1a4199e9f04796835ba7865ea64ecc232a4b6abc
carlschader-saronic <carl.schader@saronic.com>
2026-03-07 22:28:30 -0500

    sync

--- nix/nixos-modules/home-manager/tmux.nix
+++ nix/nixos-modules/home-manager/tmux.nix
@@ -15,6 +15,12 @@
           bind k select-pane -U
           bind l select-pane -R
 
+          # Vim-style pane resizing (hold to repeat)
+          bind -r H resize-pane -L 5
+          bind -r J resize-pane -D 5
+          bind -r K resize-pane -U 5
+          bind -r L resize-pane -R 5
+
           # Split panes using vim keys
           bind | split-window -h
           bind - split-window -v
