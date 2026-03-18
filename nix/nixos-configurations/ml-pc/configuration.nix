52b5c264986bc693ab33eb924b0b7379c9948bd9
carlschader <carlschader@proton.me>
2026-03-18 18:02:44 -0400

    sync

--- nix/nixos-configurations/ml-pc/configuration.nix
+++ nix/nixos-configurations/ml-pc/configuration.nix
@@ -78,9 +78,6 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-
   security.rtkit.enable = true;
 
   services.pipewire = {
