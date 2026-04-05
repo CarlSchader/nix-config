83af5df06b55883c3a59a66b2c6b1ca2e081438e
carlschader <carlschader@proton.me>
2026-04-05 18:49:22 -0400

    zathura

--- nix/home-manager/packages.nix
+++ nix/home-manager/packages.nix
@@ -1,81 +1,83 @@
 { ... }:
 {
-  homeModules.packages = { pkgs, ... }:
-  {
-    home.packages = with pkgs; [
-      ## user applications
-      ledger # cli tool for accounting
-      yubikey-manager
-      obsidian
-      brave
+  homeModules.packages =
+    { pkgs, ... }:
+    {
+      home.packages = with pkgs; [
+        ## user applications
+        ledger # cli tool for accounting
+        yubikey-manager
+        obsidian
+        brave
 
-      ## dev tools
-      git
-      neovim
-      gnumake
-      cmake
-      tmux
-      vim
-      ripgrep
-      fzf
-      kubectl
-      jq
-      zstd
-      unzip
-      pigz
-      ffmpeg
-      nmap
-      pnpm
-      gnumake
-      pwgen
-      watch
-      nix-index
-      lsof
-      direnv
-      ncdu
-      netcat
-      nload
-      jwt-cli
-      mdbook
-      gh
-      tokei
-      feh
-      protobuf
-      tree-sitter
+        ## dev tools
+        git
+        neovim
+        gnumake
+        cmake
+        tmux
+        vim
+        ripgrep
+        fzf
+        kubectl
+        jq
+        zstd
+        unzip
+        pigz
+        ffmpeg
+        nmap
+        pnpm
+        gnumake
+        pwgen
+        watch
+        nix-index
+        lsof
+        direnv
+        ncdu
+        netcat
+        nload
+        jwt-cli
+        mdbook
+        gh
+        tokei
+        feh
+        zathura
+        protobuf
+        tree-sitter
 
-      # encryption
-      sops
-      age
-      ssh-to-age
-      gnupg
-      pinentry-tty
-      openssh
-      opkssh
+        # encryption
+        sops
+        age
+        ssh-to-age
+        gnupg
+        pinentry-tty
+        openssh
+        opkssh
 
-      # ai tools
-      opencode
+        # ai tools
+        opencode
 
-      # libraries
-      boost
-      libfido2 # FIDO2 library for hardware security keys
+        # libraries
+        boost
+        libfido2 # FIDO2 library for hardware security keys
 
-      # databases
-      postgresql
+        # databases
+        postgresql
 
-      ## compilers and runtimes
-      nodejs_24
-      python312
-      luajitPackages.luarocks-nix
-      lua51Packages.lua
-			clang
+        ## compilers and runtimes
+        nodejs_24
+        python312
+        luajitPackages.luarocks-nix
+        lua51Packages.lua
+        clang
 
-      # linters
-      ruff
-      prettierd
-      nixfmt-tree
+        # linters
+        ruff
+        prettierd
+        nixfmt-tree
 
-      # packaging and project management
-      uv
-    ];
-  };
+        # packaging and project management
+        uv
+      ];
+    };
 }
