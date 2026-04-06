c5272bf018424da84f26ed52d865e934beb6fb67
carlschader <carlschader@proton.me>
2026-04-06 05:20:57 -0400

    feat: tmux works better with neovim

--- nix/home-manager/tmux.nix
+++ nix/home-manager/tmux.nix
@@ -10,27 +10,32 @@
         mouse = true;
         keyMode = "vi";
         extraConfig = ''
-                  # Use vim keys for pane navigation
-                  bind h select-pane -L
-                  bind j select-pane -D
-                  bind k select-pane -U
-                  bind l select-pane -R
+          							# Use vim keys for pane navigation
+          							bind h select-pane -L
+          							bind j select-pane -D
+          							bind k select-pane -U
+          							bind l select-pane -R
 
-                  # Vim-style pane resizing (hold to repeat)
-                  bind -r C-h resize-pane -L 5
-                  bind -r C-j resize-pane -D 5
-                  bind -r C-k resize-pane -U 5
-                  bind -r C-l resize-pane -R 5
+          							# Vim-style pane resizing (hold to repeat)
+          							bind -r C-h resize-pane -L 5
+          							bind -r C-j resize-pane -D 5
+          							bind -r C-k resize-pane -U 5
+          							bind -r C-l resize-pane -R 5
 
-                  # Reload config with prefix + r
-                  bind r source-file ~/.tmux.conf \; display "Config reloaded!"
+          							# Reload config with prefix + r
+          							bind r source-file ~/.tmux.conf \; display "Config reloaded!"
 
-                  # Split panes using vim keys
-                  bind | split-window -h
-                  bind - split-window -v
+          							# Split panes using vim keys
+          							bind | split-window -h
+          							bind - split-window -v
 
-          				# Allow copying to system clipboard with prefix + y
-          				set -s set-clipboard on
+          							# Allow copying to system clipboard with prefix + y
+          							set -s set-clipboard on
+
+          							# Neovim stuff
+          							set-option -sg escape-time 10
+          							set-option -g focus-events on
+          							set-option -g default-terminal "screen-256color"
         '';
       };
     };
