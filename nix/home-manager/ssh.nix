735e7e1ef6e75d9ba70213dfd6d34f7ee25da919
Carl Schader <carlschader@proton.me>
2026-04-12 11:28:29 -0600

    ssh home config update

--- nix/home-manager/ssh.nix
+++ nix/home-manager/ssh.nix
@@ -6,7 +6,7 @@ let
       controlPath = "~/.ssh/sockets/%r@%h-%p";
       controlPersist = "1h";
     };
-    "carl" = {
+    "ml-pc" = {
       hostname = "ml-pc";
       user = "carl";
       forwardAgent = true;
@@ -24,15 +24,23 @@ let
       forwardAgent = true;
       forwardX11 = true;
     };
+    "conifer-pi" = {
+      hostname = "conifer-pi";
+      user = "carl";
+      forwardAgent = true;
+      forwardX11 = true;
+    };
   };
 in
 {
-  homeModules.ssh = { ... }: {
-    programs.ssh = {
-      enable = true;
-      enableDefaultConfig = false;
-      inherit matchBlocks;
+  homeModules.ssh =
+    { ... }:
+    {
+      programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
+        inherit matchBlocks;
+      };
+      home.file.".ssh/sockets/.keep".text = "";
     };
-    home.file.".ssh/sockets/.keep".text = "";
-  };
 }
