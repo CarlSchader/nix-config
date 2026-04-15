a93d251a10c39f9f891c3538c04c43175ebfd7b9
carlschader <carlschader@proton.me>
2026-04-15 01:52:45 -0400

    expose-keys

--- nix/lib/default.nix
+++ nix/lib/default.nix
@@ -1,8 +1,14 @@
 { nixpkgs, ... }:
 let
-  system = "x86_64-linux"; 
-  pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
-in 
+  system = "x86_64-linux";
+  pkgs = import nixpkgs {
+    inherit system;
+    config = {
+      allowUnfree = true;
+    };
+  };
+in
 {
   nvidia-drivers."${system}" = import ./nvidia-drivers.nix;
+  ssh-keys = import ./keys.nix;
 }
