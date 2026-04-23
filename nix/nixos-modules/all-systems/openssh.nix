da12ea65096e73cc6586b1a5d904846fd88798c7
Carl Schader <carlschader@proton.me>
2026-04-23 11:35:28 -0700

    openssh allow unbinding local sockets

--- nix/nixos-modules/all-systems/openssh.nix
+++ nix/nixos-modules/all-systems/openssh.nix
@@ -10,6 +10,7 @@
           X11Forwarding = true;
           AllowAgentForwarding = true;
           PasswordAuthentication = false; # force use SSH keys instead
+          StreamLocalBindUnlink = true; # allow binding to already used socket paths for ssh-agent forwarding
         };
       };
     };
