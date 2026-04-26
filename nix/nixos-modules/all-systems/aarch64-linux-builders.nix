9a34f118bd8209ddd9351284fa2af9925979908c
carlschader <carlschader@proton.me>
2026-04-26 02:18:34 -0400

    fix: builder ssh protocol ssh-ng

--- nix/nixos-modules/all-systems/aarch64-linux-builders.nix
+++ nix/nixos-modules/all-systems/aarch64-linux-builders.nix
@@ -8,6 +8,7 @@
         {
           hostName = "129.153.207.106";
           system = "aarch64-linux";
+          protocol = "ssh-ng"; # nixbuild authorized_keys forces `nix-daemon --stdio`, which speaks the modern daemon protocol
           maxJobs = 4;
           speedFactor = 8;
           supportedFeatures = [
