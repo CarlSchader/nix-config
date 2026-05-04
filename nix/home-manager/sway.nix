0ad907063a6258eb478f2cee17dd47314943ff14
carlschader-acs <carl.schader@allencontrolsystems.com>
2026-05-03 21:19:26 -0500

    sway resize binds

--- nix/home-manager/sway.nix
+++ nix/home-manager/sway.nix
@@ -1,32 +1,48 @@
 { ... }:
 {
   homeModules.sway =
-    { pkgs, ... }:
+    {
+      pkgs,
+      config,
+      lib,
+      ...
+    }:
     {
       # programs.ghostty.enable = true;
       programs.wezterm.enable = true;
 
-      wayland.windowManager.sway = {
-        enable = true;
-        wrapperFeatures.gtk = true;
-        extraOptions = [ "--unsupported-gpu" ];
-        config = {
-          modifier = "Mod4";
-          # terminal = "ghostty";
-          terminal = "wezterm";
-          menu = "${pkgs.wofi}/bin/wofi --show drun";
-          startup = [
-            {
-              command = "dbus-update-activation-environment --systemd GNOME_KEYRING_CONTROL=/run/user/$(id -u)/keyring";
-            }
-          ];
+      wayland.windowManager.sway =
+        let
+          mod = config.wayland.windowManager.sway.config.modifier;
+        in
+        {
+          enable = true;
+          wrapperFeatures.gtk = true;
+          extraOptions = [ "--unsupported-gpu" ];
+          config = {
+            modifier = "Mod4";
+            # terminal = "ghostty";
+            terminal = "wezterm";
+            menu = "${pkgs.wofi}/bin/wofi --show drun";
+            startup = [
+              {
+                command = "dbus-update-activation-environment --systemd GNOME_KEYRING_CONTROL=/run/user/$(id -u)/keyring";
+              }
+            ];
+            keybindings = lib.mkOptionDefault {
+              "${mod}+ctrl+2" = "resize set width 50 ppt";
+              "${mod}+ctrl+3" = "resize set width 33 ppt";
+              "${mod}+ctrl+4" = "resize set width 25 ppt";
+              "${mod}+ctrl+5" = "resize set width 20 ppt";
+            };
+          };
         };
-      };
     };
 
   homeModules.sway-non-nixos =
     {
       pkgs,
+      config,
       lib,
       nixgl ? null,
       ...
@@ -79,29 +95,39 @@
         fi
       '';
 
-      wayland.windowManager.sway = {
-        enable = true;
-        package = swayPackage;
-        checkConfig = false;
-        wrapperFeatures.gtk = true;
-        extraOptions = [ "--unsupported-gpu" ];
-        config = {
-          modifier = "Mod4";
-          terminal = "wezterm-gl";
-          menu = "${pkgs.wofi}/bin/wofi --show drun";
-          bars = [
-            {
-              position = "bottom";
-              statusCommand = "${pkgs.i3status}/bin/i3status";
-              trayOutput = "*";
-            }
-          ];
-          startup = [
-            {
-              command = "dbus-update-activation-environment --systemd PATH=$HOME/.nix-profile/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin GNOME_KEYRING_CONTROL=/run/user/$(id -u)/keyring";
-            }
-          ];
+      wayland.windowManager.sway =
+        let
+          mod = config.wayland.windowManager.sway.config.modifier;
+        in
+        {
+          enable = true;
+          package = swayPackage;
+          checkConfig = false;
+          wrapperFeatures.gtk = true;
+          extraOptions = [ "--unsupported-gpu" ];
+          config = {
+            modifier = "Mod4";
+            terminal = "wezterm-gl";
+            menu = "${pkgs.wofi}/bin/wofi --show drun";
+            bars = [
+              {
+                position = "bottom";
+                statusCommand = "${pkgs.i3status}/bin/i3status";
+                trayOutput = "*";
+              }
+            ];
+            startup = [
+              {
+                command = "dbus-update-activation-environment --systemd PATH=$HOME/.nix-profile/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin GNOME_KEYRING_CONTROL=/run/user/$(id -u)/keyring";
+              }
+            ];
+            keybindings = lib.mkOptionDefault {
+              "${mod}+ctrl+2" = "resize set width 50 ppt";
+              "${mod}+ctrl+3" = "resize set width 33 ppt";
+              "${mod}+ctrl+4" = "resize set width 25 ppt";
+              "${mod}+ctrl+5" = "resize set width 20 ppt";
+            };
+          };
         };
-      };
     };
 }
