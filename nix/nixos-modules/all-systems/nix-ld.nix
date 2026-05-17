47ef67bd0677aeea27c738a653491d44985fc89a
carlschader <carlschader@proton.me>
2026-05-16 23:58:12 -0400

    nix-ld libs

--- nix/nixos-modules/all-systems/nix-ld.nix
+++ nix/nixos-modules/all-systems/nix-ld.nix
@@ -1,8 +1,61 @@
 { ... }:
 {
   nixosModules.nix-ld =
-    { ... }:
+    { pkgs, ... }:
     {
-      programs.nix-ld.enable = true;
+      programs.nix-ld = {
+        enable = true;
+        libraries = with pkgs; [
+          # Core / System
+          stdenv.cc.cc.lib
+          zlib
+          glib
+          libuuid
+          udev
+          keyutils
+
+          # Graphical / Electron Apps
+          nss
+          nspr
+          atk
+          cairo
+          pango
+          gdk-pixbuf
+          gtk3
+          libsecret
+
+          # Wayland
+          wayland
+          wayland-protocols
+          libdecor
+          xwayland
+
+          # Toolkits that bridge to Wayland
+          gtk3
+          gtk4
+          qt6.qtwayland
+
+          # X11 Windowing
+          libX11
+          libXcursor
+          libXdamage
+          libXext
+          libXfixes
+          libXi
+          libXrandr
+          libXrender
+          libXtst
+          libxcb
+          libXcomposite
+          libSM
+          libICE
+
+          # Audio / Video / 3D
+          libGL
+          alsa-lib
+          libpulseaudio
+          udev
+        ];
+      };
     };
 }
