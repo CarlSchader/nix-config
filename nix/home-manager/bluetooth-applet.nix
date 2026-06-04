3efd02cc2b62f358efc4e0f0ce706c2dfcb18269
carlschader-acs <carl.schader@allencontrolsystems.com>
2026-06-04 15:21:36 -0500

    blueman

--- nix/home-manager/bluetooth-applet.nix
+++ nix/home-manager/bluetooth-applet.nix
@@ -1,14 +1,13 @@
-{ ... }:
-{
-  homeModules.bluetooth-applet =
-    { ... }:
-    {
-      services.blueman-applet = {
-        enable = true;
-      };
-
-      services.mpris-proxy = {
-        enable = true;
-      };
+{...}: {
+  homeModules.bluetooth-applet = {pkgs, ...}: {
+    services.blueman-applet = {
+      enable = true;
     };
+
+    services.mpris-proxy = {
+      enable = true;
+    };
+
+    home.packages = with pkgs; [blueman];
+  };
 }
