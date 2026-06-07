a7f9fa4d96aa0cbfab6c0eab719f0348dd1c99d7
Carl Schader <carlschader@proton.me>
2026-06-07 12:11:27 -0700

    fix: ssh sockets %h -> %n

--- nix/home-manager/acs-ssh.nix
+++ nix/home-manager/acs-ssh.nix
@@ -1,54 +1,54 @@
-{ ... }:
-let
+{...}: let
   keys = import ../lib/keys.nix;
-in
-{
-  homeModules.acs-ssh =
-    { pkgs, lib, ... }:
-    {
-      programs.ssh = {
-        enable = true;
-        enableDefaultConfig = false;
-        settings = {
-          "*" = {
-            ControlMaster = "auto";
-            ControlPath = "~/.ssh/sockets/%r@%h-%p";
-            ControlPersist = "1h";
-          };
-          "desktop-cisco" = {
-            HostName = "10.0.20.145";
-            User = "carl";
-            ForwardAgent = true;
-            ForwardX11 = true;
-            # identityFile = "~/.ssh/id_ed25519";
-          };
-          "desktop-ts" = {
-            HostName = "100.64.0.18";
-            User = "carl";
-            ForwardAgent = true;
-            ForwardX11 = true;
-            # identityFile = "~/.ssh/id_ed25519";
-          };
-          "ml-pc" = {
-            HostName = "100.64.0.5";
-            ForwardAgent = true;
-            ForwardX11 = true;
-          };
+in {
+  homeModules.acs-ssh = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      settings = {
+        "*" = {
+          ControlMaster = "auto";
+          ControlPath = "~/.ssh/sockets/%r@%n-%p";
+          ControlPersist = "1h";
+        };
+        "desktop-cisco" = {
+          HostName = "10.0.20.145";
+          User = "carl";
+          ForwardAgent = true;
+          ForwardX11 = true;
+          # identityFile = "~/.ssh/id_ed25519";
+        };
+        "desktop-ts" = {
+          HostName = "100.64.0.18";
+          User = "carl";
+          ForwardAgent = true;
+          ForwardX11 = true;
+          # identityFile = "~/.ssh/id_ed25519";
+        };
+        "ml-pc" = {
+          HostName = "100.64.0.5";
+          ForwardAgent = true;
+          ForwardX11 = true;
         };
       };
-
-      home.file.".ssh/sockets/.keep".text = "";
-
-      home.packages = [ pkgs.x11_ssh_askpass ];
-
-      home.sessionVariables = {
-        SSH_ASKPASS = "${pkgs.x11_ssh_askpass}/libexec/x11-ssh-askpass";
-        # Often needed to trigger the popup correctly in headless-ish environments
-        SSH_ASKPASS_REQUIRE = "prefer";
-      };
-
-      home.file.".ssh/authorized_keys" = {
-        text = lib.concatStringsSep "\n" (keys.carl ++ keys.acs);
-      };
     };
+
+    home.file.".ssh/sockets/.keep".text = "";
+
+    home.packages = [pkgs.x11_ssh_askpass];
+
+    home.sessionVariables = {
+      SSH_ASKPASS = "${pkgs.x11_ssh_askpass}/libexec/x11-ssh-askpass";
+      # Often needed to trigger the popup correctly in headless-ish environments
+      SSH_ASKPASS_REQUIRE = "prefer";
+    };
+
+    home.file.".ssh/authorized_keys" = {
+      text = lib.concatStringsSep "\n" (keys.carl ++ keys.acs);
+    };
+  };
 }
--- nix/home-manager/ssh.nix
+++ nix/home-manager/ssh.nix
@@ -1,82 +1,79 @@
-{ ... }:
-{
-  homeModules.ssh =
-    { pkgs, ... }:
-    {
-      programs.ssh = {
-        enable = true;
-        enableDefaultConfig = false;
-        settings = {
-          "*" = {
-            ControlMaster = "auto";
-            ControlPath = "~/.ssh/sockets/%r@%h-%p";
-            ControlPersist = "1h";
-          };
-          "ml-pc" = {
-            HostName = "ml-pc";
-            User = "carl";
-            ForwardAgent = true;
-            ForwardX11 = true;
-          };
-          "macbook-pro-m1" = {
-            Hostname = "macbook-pro-m1";
-            User = "carlschader";
-            ForwardAgent = true;
-            ForwardX11 = true;
-          };
-          "linode-headscale" = {
-            HostName = "198.58.104.63";
-            User = "root";
-            ForwardAgent = true;
-            ForwardX11 = true;
-          };
-          "ampere-a1" = {
-            HostName = "129.153.207.106";
-            User = "carl";
-            ForwardAgent = true;
-            ForwardX11 = true;
-          };
-          "remarkable" = {
-            HostName = "192.168.0.106";
-            User = "root";
-            ForwardAgent = true;
-            ForwardX11 = true;
-          };
-          "conifer-pi" = {
-            HostName = "conifer-pi";
-            User = "carl";
-            ForwardAgent = true;
-            ForwardX11 = true;
-          };
-          "rpi*" = {
-            User = "pi";
-            ForwardAgent = true;
-            ForwardX11 = true;
-          };
-          "github-acs" = {
-            HostName = "github.com";
-            User = "git";
-            IdentityFile = "~/.ssh/acs";
-            IdentitiesOnly = true;
-          };
-          "acs-desktop" = {
-            HostName = "acs-desktop";
-            User = "carl";
-            IdentityFile = "~/.ssh/acs";
-            ForwardAgent = true;
-            ForwardX11 = true;
-          };
+{...}: {
+  homeModules.ssh = {pkgs, ...}: {
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      settings = {
+        "*" = {
+          ControlMaster = "auto";
+          ControlPath = "~/.ssh/sockets/%r@%n-%p";
+          ControlPersist = "1h";
+        };
+        "ml-pc" = {
+          HostName = "ml-pc";
+          User = "carl";
+          ForwardAgent = true;
+          ForwardX11 = true;
+        };
+        "macbook-pro-m1" = {
+          Hostname = "macbook-pro-m1";
+          User = "carlschader";
+          ForwardAgent = true;
+          ForwardX11 = true;
+        };
+        "linode-headscale" = {
+          HostName = "198.58.104.63";
+          User = "root";
+          ForwardAgent = true;
+          ForwardX11 = true;
+        };
+        "ampere-a1" = {
+          HostName = "129.153.207.106";
+          User = "carl";
+          ForwardAgent = true;
+          ForwardX11 = true;
+        };
+        "remarkable" = {
+          HostName = "192.168.0.106";
+          User = "root";
+          ForwardAgent = true;
+          ForwardX11 = true;
+        };
+        "conifer-pi" = {
+          HostName = "conifer-pi";
+          User = "carl";
+          ForwardAgent = true;
+          ForwardX11 = true;
+        };
+        "rpi*" = {
+          User = "pi";
+          ForwardAgent = true;
+          ForwardX11 = true;
+        };
+        "github-acs" = {
+          HostName = "github.com";
+          User = "git";
+          IdentityFile = "~/.ssh/acs";
+          IdentitiesOnly = true;
+        };
+        "acs-desktop" = {
+          HostName = "acs-desktop";
+          User = "carl";
+          IdentityFile = "~/.ssh/acs";
+          ForwardAgent = true;
+          ForwardX11 = true;
         };
       };
-
-      home.file.".ssh/sockets/.keep".text = "";
-
-      home.packages = [ pkgs.x11_ssh_askpass ];
-
-      home.sessionVariables = {
-        SSH_ASKPASS = "${pkgs.x11_ssh_askpass}/libexec/x11-ssh-askpass";
-        # Often needed to trigger the popup correctly in headless-ish environments
-        SSH_ASKPASS_REQUIRE = "prefer";
-      };
     };
+
+    home.file.".ssh/sockets/.keep".text = "";
+
+    home.packages = [pkgs.x11_ssh_askpass];
+
+    home.sessionVariables = {
+      SSH_ASKPASS = "${pkgs.x11_ssh_askpass}/libexec/x11-ssh-askpass";
+      # Often needed to trigger the popup correctly in headless-ish environments
+      SSH_ASKPASS_REQUIRE = "prefer";
+    };
+  };
 }
