29376112d1a4d85683714e1ab529ac25f489d132
carlschader-acs <carl.schader@allencontrolsystems.com>
2026-06-15 11:07:17 -0500

    revert-compression-level

--- acs/ssh.nix
+++ acs/ssh.nix
@@ -25,7 +25,6 @@ in {
           ForwardX11Trusted = true;
           ForwardX11 = true;
           Compression = true;
-          CompressionLevel = 9;
           # identityFile = "~/.ssh/id_ed25519";
         };
         "ml-pc" = {
