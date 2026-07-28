2187d9e23dedf5200558ddc9f8a85640d9efba8c
carlschader-acs <carl.schader@allencontrolsystems.com>
2026-07-28 16:30:18 -0500

    feat: librewolf default browser

--- librewolf/default.nix
+++ librewolf/default.nix
@@ -5,6 +5,21 @@
       profiles.carl = (import ./profiles/carl.nix) // {isDefault = true;};
       profiles.acs = import ./profiles/acs.nix;
     };
+
+    # Set librewolf as default browser
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "librewolf.desktop";
+        "x-scheme-handler/http" = "librewolf.desktop";
+        "x-scheme-handler/https" = "librewolf.desktop";
+        "x-scheme-handler/about" = "librewolf.desktop";
+        "x-scheme-handler/unknown" = "librewolf.desktop";
+        "application/xhtml+xml" = "librewolf.desktop";
+      };
+    };
+
+    home.sessionVariables.BROWSER = "librewolf";
   };
 
   homeModules.librewolf-acs = {...}: {
@@ -13,5 +28,20 @@
       profiles.carl = (import ./profiles/carl.nix) // {isDefault = false;};
       profiles.acs = (import ./profiles/acs.nix) // {isDefault = true;};
     };
+
+    # Set librewolf as default browser
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "librewolf.desktop";
+        "x-scheme-handler/http" = "librewolf.desktop";
+        "x-scheme-handler/https" = "librewolf.desktop";
+        "x-scheme-handler/about" = "librewolf.desktop";
+        "x-scheme-handler/unknown" = "librewolf.desktop";
+        "application/xhtml+xml" = "librewolf.desktop";
+      };
+    };
+
+    home.sessionVariables.BROWSER = "librewolf";
   };
 }
--- nix/home-manager/preamble.nix
+++ nix/home-manager/preamble.nix
@@ -2,5 +2,8 @@
   homeModules.preamble = {...}: {
     home.stateVersion = "26.11";
     programs.home-manager.enable = true;
+
+    # force update mimeapps
+    xdg.configFile."mimeapps.list".force = true;
   };
 }
