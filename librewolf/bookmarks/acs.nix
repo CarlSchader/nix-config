a9fef1ed19d143d5888a09d23914eab9f0d39db8
carlschader-acs <carl.schader@allencontrolsystems.com>
2026-07-31 13:06:00 -0500

    aws bookmarks

--- librewolf/bookmarks/acs.nix
+++ librewolf/bookmarks/acs.nix
@@ -47,6 +47,14 @@
           name = "s3";
           url = "https://us-east-2.console.aws.amazon.com/s3/home?region=us-east-2#";
         }
+        {
+          name = "s3";
+          url = "https://us-east-2.console.aws.amazon.com/cloudwatch/home?region=us-east-2#logsV2:log-groups";
+        }
+        {
+          name = "s3";
+          url = "https://us-east-2.console.aws.amazon.com/apigateway/main/apis?region=us-east-2";
+        }
       ];
     }
   ];
