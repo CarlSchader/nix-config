2d6a9f1896b9b9e965c7ae18cff129a626509403
Carl Schader <carlschader@proton.me>
2026-08-02 12:04:30 -0500

    feat: nixbuild user accepts carl ssh keys now

--- nix/nixos-modules/aarch64-linux/users.nix
+++ nix/nixos-modules/aarch64-linux/users.nix
@@ -1,43 +1,34 @@
-{
-  self,
-  ...
-}:
-let
+{self, ...}: let
   keys = import ../../lib/keys.nix;
   system = "aarch64-linux";
-in
-{
-  nixosModules."${system}-carl-user" =
-    { pkgs, ... }:
-    {
-      users.defaultUserShell = pkgs.zsh;
-      programs.zsh.enable = true;
-      users.users.carl = {
-        isNormalUser = true;
-        description = "Carl Schader";
-        extraGroups = [
-          "networkmanager"
-          "wheel"
-        ];
-        hashedPassword = "$6$oUx4cxq3vxiY2Czo$RdM1s7eLdnNgFV9F4UAOCFiZrkiIyW6MuIcXveRqYFmK5GrzVmvwwfkGhMNPgKk3wwu.Ybhh.//JJBPIEj/2K0";
-        packages = self.common.${system}.user-packages;
-        openssh.authorizedKeys.keys = keys.carl;
-      };
+in {
+  nixosModules."${system}-carl-user" = {pkgs, ...}: {
+    users.defaultUserShell = pkgs.zsh;
+    programs.zsh.enable = true;
+    users.users.carl = {
+      isNormalUser = true;
+      description = "Carl Schader";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+      ];
+      hashedPassword = "$6$oUx4cxq3vxiY2Czo$RdM1s7eLdnNgFV9F4UAOCFiZrkiIyW6MuIcXveRqYFmK5GrzVmvwwfkGhMNPgKk3wwu.Ybhh.//JJBPIEj/2K0";
+      packages = self.common.${system}.user-packages;
+      openssh.authorizedKeys.keys = keys.carl;
     };
+  };
 
-  nixosModules."${system}-nixbuild-user" =
-    { pkgs, ... }:
-    {
-      users.defaultUserShell = pkgs.zsh;
-      programs.zsh.enable = true;
-      users.users.nixbuild = {
-        isNormalUser = true;
-        description = "builder user for nix distributed builds";
-        extraGroups = [
-          "kvm"
-        ];
-        openssh.authorizedKeys.keys = keys.nixbuild;
-      };
-      nix.settings.trusted-users = [ "nixbuild" ];
+  nixosModules."${system}-nixbuild-user" = {pkgs, ...}: {
+    users.defaultUserShell = pkgs.zsh;
+    programs.zsh.enable = true;
+    users.users.nixbuild = {
+      isNormalUser = true;
+      description = "builder user for nix distributed builds";
+      extraGroups = [
+        "kvm"
+      ];
+      openssh.authorizedKeys.keys = keys.nixbuild ++ keys.carl;
     };
+    nix.settings.trusted-users = ["nixbuild"];
+  };
 }
