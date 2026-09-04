d94ec293bfaf21b83394d115ccf940d4109f730e
Carl Schader <carlschader@proton.me>
2026-09-04 02:15:15 -0500

    feat: vllm bookmark

--- librewolf/bookmarks/ai.nix
+++ librewolf/bookmarks/ai.nix
@@ -6,6 +6,10 @@
       name = "ai";
       bookmarks = [
         {
+          name = "vllm";
+          url = "http://rtx4090-tower:8080";
+        }
+        {
           name = "claude";
           url = "https://claude.ai/new";
         }
