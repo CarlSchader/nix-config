a38d1d2d3078b1e94372bb2f001865fe060592e9
carlschader-acs <carl.schader@allencontrolsystems.com>
2026-09-09 04:07:27 -0400

    feat: pi permissions on git and working in external dirs

--- pi/home-modules.nix
+++ pi/home-modules.nix
@@ -1,5 +1,6 @@
 {self, ...}: {
   homeModules.pi-coding-agent = {
+    pkgs,
     config,
     lib,
     ...
@@ -24,23 +25,37 @@
       };
     };
 
-    config.programs.pi-coding-agent = {
-      enable = true;
-      models = config.my.pi-coding-agent.models;
-      settings = {
-        defaultProvider = config.my.pi-coding-agent.defaultProvider;
-        defaultModel = config.my.pi-coding-agent.defaultModel;
-        packages = [
-          "npm:pi-mcp-adapter"
-          "npm:pi-web-access"
-          "npm:pi-subagents"
-          "npm:@juicesharp/rpiv-ask-user-question"
-          "npm:@juicesharp/rpiv-todo"
-          # "npm:context-mode"
-          # "npm:pi-lens"
-          # "npm:@plannotator/pi-extension"
-          "npm:@dreki-gg/pi-ask-mode"
-        ];
+    config = {
+      programs.pi-coding-agent = {
+        enable = true;
+        models = config.my.pi-coding-agent.models;
+        settings = {
+          defaultProvider = config.my.pi-coding-agent.defaultProvider;
+          defaultModel = config.my.pi-coding-agent.defaultModel;
+          packages = [
+            "npm:pi-mcp-adapter"
+            "npm:pi-web-access"
+            "npm:pi-subagents"
+            "npm:@juicesharp/rpiv-ask-user-question"
+            "npm:@juicesharp/rpiv-todo"
+            # "npm:context-mode"
+            # "npm:pi-lens"
+            # "npm:@plannotator/pi-extension"
+            "npm:@dreki-gg/pi-ask-mode"
+            "npm:@gotgenes/pi-permission-system"
+          ];
+        };
+      };
+
+      home.file.".pi/agent/extensions/pi-permission-system/config.json".source = (pkgs.formats.json {}).generate "pi-permission-config.json" {
+        permission = {
+          external_directory = "ask";
+          bash = {
+            "*" = "allow";
+            "git commit *" = "ask";
+            "git push *" = "ask";
+          };
+        };
       };
     };
   };
